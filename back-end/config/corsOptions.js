74b3659c33ddfe5efeff29454ba5fd93f9b81939
rahuleth <rahulsaini593@gmail.com>
2025-03-06 18:45:46 +0530

    APIs

---
+++ back-end/config/corsOptions.js
@@ -0,0 +1,25 @@
+const allowdedOrigins = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://localhost:4000",
+    "chrome-extension://*", // replace star with your extension ID
+  ];
+  
+  const corsOptions = {
+    origin: (origin, callback) => {
+      if (
+        allowdedOrigins.includes(origin) ||
+        origin.startsWith("chrome-extension://")
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS Error: Not Allowded"));
+      }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200,
+  };
+  
+  module.exports = corsOptions;
+  
